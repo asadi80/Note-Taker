39bdb6a4bcf292ef77a20d4b222db9d283f66658
asdi80 <a8sadi@gmail.com>
2022-01-04 13:09:40 -0700

    done with the third stage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { tasks } = require('./db/db');
 
 const PORT = process.env.PORT || 3000;
@@ -66,6 +67,18 @@ app.post('/api/db', (req, res) => {
     res.json(req.body);
   }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.listen(PORT, () => {
 console.log(`API server now on port ${PORT}!`);
